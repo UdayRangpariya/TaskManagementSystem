24dcc3ab89f33e010b46544dd6f6903d9600fe0d
UdayRangpariya <udayrangpariya5896@gmail.com>
2025-03-14 15:08:51 +0530

    Authorization Completed for login

--- API/Controller/AuthController.cs
+++ API/Controller/AuthController.cs
@@ -1,7 +1,11 @@
 using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
 using Repositories.Interface;
 using Repositories.Model;
 
@@ -12,10 +16,12 @@ namespace API.Controller
     public class AuthController : ControllerBase
     {
         private readonly IAuth _authService;
+        private readonly IConfiguration _configuration;
 
-        public AuthController(IAuth authService)
+        public AuthController(IAuth authService, IConfiguration configuration)
         {
             _authService = authService;
+            _configuration = configuration;
         }
 
         [HttpPost("register")]
@@ -25,19 +31,24 @@ namespace API.Controller
             {
                 return BadRequest(ModelState);
             }
-            
+
             try
             {
                 var user = await _authService.RegisterUserAsync(model);
-                return Ok(new { 
-                    message = "Registration successful", 
-                    user = new { 
+
+                // Don't include sensitive information in the response
+                return Ok(new
+                {
+                    message = "Registration successful",
+                    user = new
+                    {
                         userId = user.UserId,
                         username = user.Username,
                         email = user.Email,
                         role = user.Role,
                         firstName = user.FirstName,
                         lastName = user.LastName
+                        // Notice password is not included here
                     }
                 });
             }
@@ -54,24 +65,48 @@ namespace API.Controller
             {
                 return BadRequest(ModelState);
             }
-            
+
             try
             {
                 var user = await _authService.LoginUserAsync(model);
+
+                if (user != null && user.UserId != 0)
+                {
+                    var claims = new[]
+                    {
+                        new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+                        new Claim("Userid", user.UserId.ToString()),
+                        new Claim("UserName", user.Username)
+                    };
+
+                    var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["Jwt:Key"]));
+                    var signIn = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+                    var token = new JwtSecurityToken(
+                        issuer: _configuration["Jwt:Issuer"],
+                        audience: _configuration["Jwt:Audience"],
+                        claims: claims,
+                        expires: DateTime.UtcNow.AddDays(1),
+                        signingCredentials: signIn
+                    );
+
+                    return Ok(new
+                    {
+                        success = true,
+                        message = "Login successful",
+                        user = new
+                        {
+                            userId = user.UserId,
+                            username = user.Username,
+                            email = user.Email,
+                            role = user.Role,
+                            firstName = user.FirstName,
+                            lastName = user.LastName
+                        },
+                        token = new JwtSecurityTokenHandler().WriteToken(token)
+                    });
+                }
                 
-                // In a real application, you would generate a JWT token here
-                
-                return Ok(new { 
-                    message = "Login successful", 
-                    user = new { 
-                        userId = user.UserId,
-                        username = user.Username,
-                        email = user.Email,
-                        role = user.Role,
-                        firstName = user.FirstName,
-                        lastName = user.LastName
-                    }
-                });
+                return Unauthorized(new { message = "Invalid login attempt" });
             }
             catch (Exception ex)
             {
--- API/Program.cs
+++ API/Program.cs
@@ -1,4 +1,8 @@
-
+using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.Net.Http.Headers;
+using Microsoft.OpenApi.Models;
 using Npgsql;
 using Repositories.Implementation;
 using Repositories.Interface;
@@ -8,7 +12,54 @@ var builder = WebApplication.CreateBuilder(args);
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+
+builder.Services.AddAuthentication(option =>
+{
+    option.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    option.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+    option.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+}
+).AddJwtBearer(options =>
+{
+    options.RequireHttpsMetadata = false;
+    options.SaveToken = true;
+    options.TokenValidationParameters = new TokenValidationParameters()
+    {
+        ValidateIssuer = false,
+        ValidateAudience = false,
+        ValidateLifetime = false,
+        ValidAudience = builder.Configuration["Jwt:Audience"],
+        ValidIssuer = builder.Configuration["Jwt:Issuer"],
+        IssuerSigningKey = new
+   SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]))
+    };
+});
+
+builder.Services.AddSwaggerGen(c =>
+{
+    c.AddSecurityDefinition(
+        "token",
+        new OpenApiSecurityScheme
+        {
+            Type = SecuritySchemeType.Http,
+            BearerFormat = "JWT",
+            Scheme = "Bearer",
+            In = ParameterLocation.Header,
+            Name = HeaderNames.Authorization
+        }
+    );
+    c.AddSecurityRequirement(
+        new OpenApiSecurityRequirement {
+            {   new OpenApiSecurityScheme {
+                    Reference = new OpenApiReference {
+                        Type = ReferenceType.SecurityScheme,
+                        Id = "token"
+                    },
+                },Array.Empty<string>()
+            }
+        }
+    );
+});
 // Get the connection string
 var connectionString = builder.Configuration.GetConnectionString("pgconn");
 // Register Auth service with the connection string parameter
@@ -22,6 +73,10 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseAuthentication();
+app.UseAuthorization();
+
 app.MapControllers();
 app.UseHttpsRedirection();
 
